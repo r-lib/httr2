a5c1bbe2c47334b66a5ee620913190b80b998520
Hadley Wickham <h.wickham@gmail.com>
2021-05-25 11:33:07 -0500

    Condition snapshots on libcurl version

--- tests/testthat/test-req.R
+++ tests/testthat/test-req.R
@@ -1,3 +1,4 @@
 test_that("req has basic print method", {
+  skip_if_not(curl::curl_version()$version == "7.64.1")
   expect_snapshot(req("https://r-project.org"))
 })
--- tests/testthat/test-resp.R
+++ tests/testthat/test-resp.R
@@ -1,3 +1,4 @@
 test_that("respinse has basic print method", {
+  skip_if_not(curl::curl_version()$version == "7.64.1")
   expect_snapshot(req_fetch(req("https://httpbin.org")))
 })
