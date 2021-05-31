41ee40d40405f7e45b0f24362531356b77abc39c
Hadley Wickham <h.wickham@gmail.com>
2021-05-31 15:04:23 -0500

    Use webfakes for testing endpoint

---
+++ R/test.R
@@ -0,0 +1,9 @@
+req_test <- function(template = "GET /get", ...) {
+  if (is.null(the$test_app)) {
+    the$test_app <- webfakes::new_app_process(webfakes::httpbin_app())
+  }
+
+  req <- req(the$test_app$url())
+  req <- req_template(req, template, ...)
+  req
+}
