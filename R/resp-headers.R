59cd7fb3214b5a5453b7eab0ec839a4ab521c3dd
Hadley Wickham <h.wickham@gmail.com>
2021-05-31 23:45:30 -0500

    Document header helpers

--- R/resp-headers.R
+++ R/resp-headers.R
@@ -1,8 +1,27 @@
 #' Extract headers from a response
 #'
+#' @description
+#' Some of these functions provide generic access to all headers:
 #' * `resp_headers()` retrieves a list of all headers.
 #' * `resp_header()` retrieves a single header.
-#' * `resp_header_exists()` checks if a header is present
+#' * `resp_header_exists()` checks if a header is present.
+#'
+#' Other functions parse specific headers:
+#'
+#' * `resp_content_type()` returns the just the type and subtype of the
+#'    from the `Content-Type` header. If `Content-Type` is not provided
+#'    (or is not a valid mime type), this returns `NA`. Used by
+#'    [resp_body_json()], [resp_body_html()], and [resp_body_xml()].
+#'
+#' * `resp_date()` returns the `Date` header as a POSIXct. This header
+#'    always exists; if the server does not return it, httr2 adds
+#'    automatically.
+#'
+#' *  `resp_encoding()` returns the likely character encoding of text
+#'    types, as parsed from the `charset` parameter of the `Content-Type`
+#'    header. If that header is not found, not valid, or no charset parameter
+#'    is found, this returns `UTF-8`. Used by [resp_body_string()].
+#'
 #'
 #' @param resp An HTTP response object, as created by [req_fetch()].
 #' @export
@@ -26,14 +45,20 @@ resp_header_exists <- function(resp, header) {
   has_name(resp$headers, tolower(header))
 }
 
+#' @export
+#' @rdname resp_headers
 resp_content_type <- function(resp) {
   resp_parsed_content_type(resp)$complete %||% NA_character_
 }
 
+#' @export
+#' @rdname resp_headers
 resp_date <- function(resp) {
   httr::parse_http_date(resp_header(resp, "Date"), NULL)
 }
 
+#' @export
+#' @rdname resp_headers
 resp_encoding <- function(resp) {
   resp_parsed_content_type(resp)$params$charset %||% "UTF-8"
 }
