a787e39e1de0006d2bdfe3d3c12181ebddf3ea63
Hadley Wickham <h.wickham@gmail.com>
2021-05-31 20:01:27 -0500

    Add more tests for utils

---
+++ tests/testthat/_snaps/utils.md
@@ -0,0 +1,22 @@
+# modify list adds, removes, and overrides
+
+    Code
+      modify_list(x, a = 1, 2)
+    Error <rlang_error>
+      All components of ... must be named
+
+# can check arg types
+
+    Code
+      check_string(1, "x")
+    Error <rlang_error>
+      x must be a string
+    Code
+      check_number("2", "x")
+    Error <rlang_error>
+      {name} must be a number
+    Code
+      check_number(NA_real_, "x")
+    Error <rlang_error>
+      {name} must be a number
+
---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,18 @@
+test_that("modify list adds, removes, and overrides", {
+  x <- list(x = 1)
+  expect_equal(modify_list(x), x)
+  expect_equal(modify_list(x, x = NULL), list())
+  expect_equal(modify_list(x, x = 2), list(x = 2))
+  expect_equal(modify_list(x, y = 3), list(x = 1, y = 3))
+  expect_equal(modify_list(NULL, x = 2), list(x = 2))
+
+  expect_snapshot(modify_list(x, a = 1, 2), error = TRUE)
+})
+
+test_that("can check arg types", {
+  expect_snapshot(error = TRUE, {
+    check_string(1, "x")
+    check_number("2", "x")
+    check_number(NA_real_, "x")
+  })
+})
