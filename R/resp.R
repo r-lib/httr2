b0e82aa091b3d41936270e920783fefd4f681375
Hadley Wickham <h.wickham@gmail.com>
2021-06-08 21:19:15 -0500

    Document + export response

--- R/resp.R
+++ R/resp.R
@@ -1,3 +1,40 @@
+#' Create a new HTTP response
+#'
+#' Generally, you should not need to call this function directly; you'll
+#' get a real HTTP response by calling [req_fetch()] and friends. This
+#' function is provided primarily for testing, and a place to describe
+#' the key components of a response.
+#'
+#' @keywords internal
+#' @param status_code HTTP status code. Must be a single integer.
+#' @param url URL response came from; might not be the same as the URL in
+#'   the request if there were any redirects.
+#' @param method HTTP method used to retrieve the response.
+#' @param headers A list of HTTP headers.
+#' @param body Response, if any, contained in the response body.
+#' @export
+#' @examples
+#' response()
+#' response(404, method = "POST")
+response <- function(status_code = 200,
+                     url = "http://example.com",
+                     method = "GET",
+                     headers = list(),
+                     body = NULL) {
+
+  if (is.character(headers)) {
+    headers <- curl::parse_headers_list(headers)
+  }
+
+  new_response(
+    method = method,
+    url = url,
+    status_code = as.integer(status_code),
+    headers = headers,
+    body = body
+  )
+}
+
 new_response <- function(method, url, status_code, headers, body, times) {
   check_string(method, "method")
   check_string(url, "url")
@@ -23,24 +60,6 @@ new_response <- function(method, url, status_code, headers, body, times) {
   )
 }
 
-response <- function(status_code = 200,
-                     url = "http://example.com",
-                     method = "GET",
-                     headers = list(),
-                     body = NULL) {
-
-  if (is.character(headers)) {
-    headers <- curl::parse_headers_list(headers)
-  }
-
-  new_response(
-    method = method,
-    url = url,
-    status_code = as.integer(status_code),
-    headers = headers,
-    body = body
-  )
-}
 
 #' @export
 print.httr2_response <- function(x,...) {
