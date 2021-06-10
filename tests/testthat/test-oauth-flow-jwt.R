70a3bfd3792630001aac586b524fd8300dd2bdde
Hadley Wickham <h.wickham@gmail.com>
2021-06-09 23:54:10 -0500

    Collapse tests since have so much in common

--- tests/testthat/test-oauth-flow-jwt.R
+++ tests/testthat/test-oauth-flow-jwt.R
@@ -4,35 +4,24 @@
 # 4. secret_write_rds(json, "tests/testthat/test-oauth-flow-jwt-google.rds",
 #      secret_get_key("HTTR2_KEY"))
 
-test_that("can generate token with oauth_flow_jwt", {
+test_that("can generate token and use it automatically", {
   secrets <- secret_read_rds(test_path("test-oauth-flow-jwt-google.rds"), "HTTR2_KEY")
 
   app <- oauth_app(
     client = oauth_client(secrets$client_id),
     endpoints = c(token = secrets$token_uri)
   )
-  claims <- jwt_claim_set(
+  claims <- list2(
     iss = secrets$client_email,
     scope = "https://www.googleapis.com/auth/userinfo.email",
     aud = "https://oauth2.googleapis.com/token"
   )
+
+  # Can generate token
   token <- oauth_flow_jwt(app, claims, "jwt_sign_rs256", list(private_key = secrets$private_key))
   expect_s3_class(token, "httr2_token")
-})
 
-test_that("can authenticate request with req_oauth_jwt", {
-  secrets <- secret_read_rds(test_path("test-oauth-flow-jwt-google.rds"), "HTTR2_KEY")
-
-  app <- oauth_app(
-    client = oauth_client(secrets$client_id),
-    endpoints = c(token = secrets$token_uri)
-  )
-  claims <- list(
-    iss = secrets$client_email,
-    scope = "https://www.googleapis.com/auth/userinfo.email",
-    aud = "https://oauth2.googleapis.com/token"
-  )
-
+  # Can use it in request
   resp <- request("https://openidconnect.googleapis.com/v1/userinfo") %>%
     req_oauth_jwt(app, claims, "jwt_sign_rs256", list(private_key = secrets$private_key)) %>%
     req_fetch() %>%
