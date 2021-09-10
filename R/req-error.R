d659e1fccb3b4fc5f552239e3d94bc76d0e5b3ec
Hadley Wickham <h.wickham@gmail.com>
2021-09-10 09:52:54 -0500

    Handling erroring error callback (#69)
    Fixes #63

--- R/req-error.R
+++ R/req-error.R
@@ -33,5 +33,19 @@ error_is_error <- function(req, resp) {
 }
 
 error_body <- function(req, resp) {
-  req_policy_call(req, "error_body", list(resp), default = NULL)
+  # TODO: revisit once rlang has better support for this
+  tryCatch(
+    req_policy_call(req, "error_body", list(resp), default = NULL),
+    error = function(cnd) {
+      msg <- c(
+        "",
+        "Additionally, req_error(body = ) failed with error:",
+        gsub("\n", "\n  ", conditionMessage(cnd))
+      )
+      if (utils::packageVersion("rlang") >= "0.4.11.9001") {
+        names(msg)[[3]] <- " "
+      }
+      msg
+    }
+  )
 }
--- tests/testthat/test-req-error.R
+++ tests/testthat/test-req-error.R
@@ -15,3 +15,14 @@ test_that("can customise error info", {
   req <- req %>% req_error(body = ~ "Hi!")
   expect_equal(error_body(req, response(404)), "Hi!")
 })
+
+test_that("failing callback still generates useful body", {
+  req <- request_test() %>% req_error(body = ~ abort("This is an error!"))
+  expect_snapshot_output(error_body(req, response(404)))
+
+  expect_snapshot(error = TRUE, {
+    req <- request("https://httpbin.org/status/404")
+    req <- req %>% req_error(body = ~ resp_body_json(.x)$error)
+    req %>% req_perform()
+  })
+})
