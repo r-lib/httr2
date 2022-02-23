76eb905681358a9c9bedb3e656bbff1bf3014107
owenjonespp <owen.jones@purepla.net>
2022-02-23 17:10:10 +0000

    Move req_proxy() to req-options

--- R/req-options.R
+++ R/req-options.R
@@ -84,6 +84,41 @@ req_timeout <- function(req, seconds) {
   req_options(req, timeout_ms = seconds * 1000)
 }
 
+
+#' Use a proxy for a request
+#'
+#' @inheritParams req_perform
+#' @param url,port Location of proxy.
+#' @param username,password Login details for proxy, if needed.
+#' @param auth Type of HTTP authentication to use. Should be one of the
+#'   following: basic, digest, digest_ie, gssnegotiate, ntlm, any.
+#' @examples
+#' # See http://www.hidemyass.com/proxy-list for a list of public proxies
+#' # to test with
+#' # request("http://had.co.nz") %>%
+#' #   req_proxy("64.251.21.73", 8080) %>%
+#' #   req_perform()
+#' @export
+req_proxy <- function(req, url, port = NULL, username = NULL, password = NULL, auth = "basic") {
+
+  if (!is.null(username) || !is.null(password)) {
+    proxyuserpwd <- paste0(username, ":", password)
+  } else {
+    proxyuserpwd <- NULL
+  }
+
+  if (!is.null(port)) stopifnot(is.numeric(port))
+
+  req_options(
+    req,
+    proxy = url,
+    proxyport = port,
+    proxyuserpwd = proxyuserpwd,
+    proxyauth = auth_flags(auth)
+  )
+}
+
+
 #' Show extra output when request is performed
 #'
 #' @description
@@ -142,6 +177,7 @@ req_verbose <- function(req,
   req_options(req, debugfunction = debug, verbose = TRUE)
 }
 
+
 # helpers -----------------------------------------------------------------
 
 verbose_message <- function(prefix, x) {
@@ -169,4 +205,21 @@ verbose_header <- function(prefix, x, redact = TRUE) {
       cli::cat_line(prefix, line)
     }
   }
+}
+
+
+auth_flags <- function(x = "basic") {
+
+  constants <- c(
+    basic = 1,
+    digest = 2,
+    gssnegotiate = 4,
+    ntlm = 8,
+    digest_ie = 16,
+    any = -17
+  )
+
+  x <- match.arg(x, names(constants))
+
+  constants[[x]]
 }
