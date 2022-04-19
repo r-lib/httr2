c2788fcf11721b412b649385ce9d3dd829693f7c
Jakub Chromec <chromec.jakub@gmail.com>
2022-04-19 13:55:53 +0200

    one more throttle test

--- tests/testthat/test-req-throttle.R
+++ tests/testthat/test-req-throttle.R
@@ -6,6 +6,21 @@ test_that("throttling causes delay", {
   expect_true(throttle_delay(req) > 0.005)
 })
 
+test_that("20 requests at rate 4/s take ~5 seconds to run", {
+  skip_on_cran()
+  throttle_reset()
+
+  req <- request_test() %>% req_throttle(4)
+
+  # Ensure all 20 requests below will have to wait.
+  req_perform(req)
+
+  elapsed <- system.time(rep(list(req), 20) %>% lapply(req_perform))[[3]]
+
+  expect_gt(elapsed, 4)
+  expect_lt(elapsed, 6)
+})
+
 test_that("realm defaults to hostname but can be overridden", {
   throttle_reset()
   expect_equal(the$throttle, list())
