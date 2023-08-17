e196b35323f2935a30d140cb7898972c9e4a4807
Hadley Wickham <h.wickham@gmail.com>
2023-08-16 22:54:52 -0500

    Implement resp_url() helpers
    Fixes #57

---
+++ R/resp-url.R
@@ -0,0 +1,37 @@
+#' Get URL/components from the response
+#'
+#' * `resp_url()` returns the complete url.
+#' * `resp_url_path()` returns the path component.
+#' * `resp_url_query()` returns the query component as a named list.
+#'
+#' @inheritParams resp_header
+#' @export
+#' @examples
+#' req <- request("https://httr2.r-lib.org?hello=world")
+#'
+#' resp <- req_perform(req)
+#' resp %>% resp_url()
+#' resp %>% resp_url_path()
+#' resp %>% resp_url_query()
+resp_url <- function(resp) {
+  check_response(resp)
+
+  resp$url
+}
+
+#' @export
+#' @rdname resp_url
+resp_url_path <- function(resp) {
+  check_response(resp)
+
+  url_parse(resp$url)$path
+}
+
+#' @export
+#' @rdname resp_url
+resp_url_query <- function(resp) {
+  check_response(resp)
+
+  url_parse(resp$url)$query
+
+}
---
+++ tests/testthat/test-resp-url.R
@@ -0,0 +1,7 @@
+test_that("can extract url components from a response", {
+  resp <- req_perform(request_test("/get?a=1&b=2"))
+
+  expect_equal(resp_url(resp), paste0(example_url(), "get?a=1&b=2"))
+  expect_equal(resp_url_path(resp), "/get")
+  expect_equal(resp_url_query(resp), list(a = "1", b = "2"))
+})
