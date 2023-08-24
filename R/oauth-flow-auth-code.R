51a04ffbe4d87e4385ae250dbca5a0f14f08f3c1
Hadley Wickham <h.wickham@gmail.com>
2023-08-23 21:38:41 -0500

    Remove spurious change

--- R/oauth-flow-auth-code.R
+++ R/oauth-flow-auth-code.R
@@ -153,8 +153,6 @@ oauth_flow_auth_code <- function(client,
     token_params$code_verifier <- code$verifier
   }
 
-  redirect_uri <- redirect_uri %||% "http://localhost@"
-
   state <- base64_url_rand(32)
   redirect_url <- paste0("http://", host_name, ":", port, "/")
 
