d9696b08c84bf5918bd229dcda7cde4fdaf11b43
Maximilian Girlich <maximilian.girlich@metoda.com>
2023-09-01 10:11:40 +0200

    Add pagination
    * Add workaround for `req_body_json()` and content type
    
    * Add `req_paginate()`
    
    * Fix workaround
    
    * WIP
    
    * Make `req_paginate()` more lower level
    
    * Quick documentation
    
    * Export `in_query()`, `in_header()`, and `in_body()`
    
    * Refactor
    
    * Change interface to anonymous functions
    
    * Fix documentation
    
    * Actually check arguments in `check_function2()`
    
    * Add standalone cli
    
    * No need for standalone cli
    
    * Add some basic tests
    
    * Remove `calculate_n_pages()`
    
    * Improve documentation for `req_paginate()`
    
    * Rename to `paginate_req_perform()`
    
    * Link to `*_req_perform()` from `req_perform()`
    
    * Export `paginate_next_request()`
    
    * Check for pagination policy in `paginate_req_perform()`
    
    * Simplify `req_paginate_offset()`
    
    * Store offset in request
    
    * Fix example for `paginate_req_perform()`
    
    * Rename to `req_paginate_token()`
    
    * Kind of support an infinite amount of pages
    
    * Add more tests
    
    * Fix test
    
    * Avoid modern R syntax
    
    * More documentation tweaks
    
    * Add pagination to pkgdown yaml
    
    * Remove workaround
    
    * Fix pkgdown

--- R/req-perform.R
+++ R/req-perform.R
@@ -59,6 +59,9 @@
 #'   * If the HTTP request fails (e.g. the connection is dropped or the
 #'     server doesn't exist), an error with class `"httr2_failure"`.
 #' @export
+#' @seealso [multi_req_perform()] to perform multiple requests in parallel.
+#'   [paginate_req_perform()] to fetch all pages of a requests paginated via
+#'   [req_paginate()].
 #' @examples
 #' request("https://google.com") %>%
 #'   req_perform()
