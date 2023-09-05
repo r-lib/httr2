919ece27266d2221be63859866265684c826d4bb
Hadley Wickham <h.wickham@gmail.com>
2023-09-04 20:37:39 -0500

    Update args for oauth_flow_auth_code_listen()
    And remove extraenous args

--- R/oauth-flow-auth-code.R
+++ R/oauth-flow-auth-code.R
@@ -127,9 +127,6 @@ req_oauth_auth_code <- function(req, client,
 #'   `token_url`.
 #' @param host_name,host_ip,port `r lifecycle::badge("deprecated")`
 #'   Now use `redirect_uri` instead.
-#' @param host_ip IP address for the temporary webserver used to capture the
-#'   authorization code.
-#' @param port Port to bind the temporary webserver to.
 #' @param redirect_uri URL to redirect back to after authorization is complete.
 #'   Often this must be registered with the API in advance.
 #'
@@ -205,10 +202,7 @@ oauth_flow_auth_code <- function(client,
 
   if (redirect$localhost) {
     # Listen on localhost for the result
-    result <- oauth_flow_auth_code_listen(
-      port = redirect$port,
-      path = redirect$path
-    )
+    result <- oauth_flow_auth_code_listen(redirect$uri)
     code <- oauth_flow_auth_code_parse(result, state)
   } else {
     # Allow the user to retrieve the token out of band manually and enter it
@@ -275,9 +269,7 @@ normalize_redirect_uri <- function(redirect_uri,
 
   list(
     uri = url_build(parsed),
-    localhost = localhost,
-    port = parsed$port,
-    path = parsed$path %||% "/"
+    localhost = localhost
   )
 
 }
@@ -310,7 +302,11 @@ oauth_flow_auth_code_url <- function(client,
 
 #' @export
 #' @rdname oauth_flow_auth_code
-oauth_flow_auth_code_listen <- function(host_ip = "127.0.0.1", port = 1410, path = "/") {
+oauth_flow_auth_code_listen <- function(redirect_uri = "http://localhost:1410") {
+  parsed <- url_parse(redirect_uri)
+  port <- parsed$port
+  path <- parsed$path %||% "/"
+
   complete <- FALSE
   info <- NULL
   listen <- function(env) {
