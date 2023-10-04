63ef0d6aa31296d18be24fd44757b8eefcc5c01a
Maximilian Girlich <maximilian.girlich@metoda.com>
2023-10-04 06:25:44 +0000

    Improve `req_paginate()` documentation

--- R/paginate.R
+++ R/paginate.R
@@ -7,17 +7,19 @@
 #' There are also helpers for common pagination patterns:
 #'   * `req_paginate_next_url()` when the response contains a link to the next
 #'     page.
-#'   * `req_paginate_offset()` when the request describes the offset i.e.
-#'     at which element to start and the page size.
 #'   * `req_paginate_next_token()` when the response contains a token
 #'     that is used to describe the next page.
+#'   * `req_paginate_offset()` when the request describes the offset i.e.
+#'     at which element to start and the page size.
+#'   * `req_paginate_page_index()` when the request specifies which the page to
+#'     request via an index.
 #'
 #' @inheritParams req_perform
 #' @param next_request A callback function that returns a [request] to the next
 #'   page or `NULL` if there is no next page. It takes two arguments:
 #'
 #'   1. `req`: the previous request.
-#'   2. `parsed`: the result of the argument `parse_resp`.
+#'   2. `parsed`: the previous response parsed via the argument `parse_resp`.
 #' @param parse_resp A function with one argument `resp` that parses the
 #'   response and returns a list with the field `data` and other fields needed
 #'   to create the request for the next page.
@@ -31,10 +33,8 @@
 #' @param required_fields An optional character vector that specifies which
 #'   fields are required in the list returned by `parse_resp()`.
 #' @param n_pages An optional function that extracts the total number of pages, improving the
-#'   automatically generated progress bar. It has two arguments:
-#'
-#'   1. `resp`: the response of the current request.
-#'   2. `parsed`: the result of the argument `parse_resp`.
+#'   automatically generated progress bar. It has one argument `parsed`, which
+#'   is the previous response parsed via the argument `parse_resp`.
 #'
 #' @return A modified HTTP [request].
 #' @seealso [paginate_req_perform()] to fetch all pages. [paginate_next_request()]
