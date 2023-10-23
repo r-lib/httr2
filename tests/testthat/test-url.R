b8b853ea2ff70c681f2c822857c544816ecdcb7e
Hadley Wickham <h.wickham@gmail.com>
2023-10-23 17:01:35 -0500

    Trim spaces when formatting vector of numbers
    Fixes #354

--- tests/testthat/test-url.R
+++ tests/testthat/test-url.R
@@ -83,8 +83,9 @@ test_that("handles all atomic vectors", {
 })
 
 
-test_that("doubles don't use scientific notation", {
+test_that("formats numbers nicely", {
   expect_equal(format_query_param(1e9), "1000000000")
+  expect_equal(format_query_param(c(1, 1000)), c("1", "1000"))
 })
 
 test_that("can opt out of escaping", {
