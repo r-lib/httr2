a241b04b079c5a76a04e5ac449c0612c99e0fd00
Hadley Wickham <h.wickham@gmail.com>
2024-09-05 14:18:03 +0100

    Implement `req_cookies_set()` (#540)
    Fixes #369

--- tests/testthat/test-req-cookies.R
+++ tests/testthat/test-req-cookies.R
@@ -20,3 +20,21 @@ test_that("can read/write cookies", {
   expect_mapequal(cookies, list(x = "a", y = "b", z = "c"))
 
 })
+
+test_that("can set cookies", {
+  resp <- request(example_url()) %>%
+    req_cookies_set(a = 1, b = 1) %>%
+    req_url_path("/cookies") %>%
+    req_perform()
+
+  expect_equal(resp_body_json(resp), list(cookies = list(a = "1", b = "1")))
+})
+
+test_that("cookie values are usually escaped", {
+  resp <- request(example_url()) %>%
+    req_cookies_set(a = I("%20"), b = "%") %>%
+    req_url_path("/cookies") %>%
+    req_perform()
+
+  expect_equal(resp_body_json(resp), list(cookies = list(a = "%20", b = "%25")))
+})
