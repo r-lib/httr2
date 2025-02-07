249dc15fdba532fe320b1df08c1d51afe6853a71
Hadley Wickham <h.wickham@gmail.com>
2025-02-07 11:54:45 -0600

    `example_url()` improvements
    Fixes #663

--- tests/testthat/test-iterate.R
+++ tests/testthat/test-iterate.R
@@ -1,6 +1,5 @@
 test_that("can perform multiple requests", {
-  req <- request(example_url()) %>%
-    req_url_path("/iris") %>%
+  req <- request(example_url("/iris")) %>%
     req_url_query(limit = 5)
 
   resps <- req_perform_iterative(
@@ -14,8 +13,7 @@ test_that("can perform multiple requests", {
 })
 
 test_that("can save results to disk", {
-  req <- request(example_url()) %>%
-    req_url_path("/iris") %>%
+  req <- request(example_url("/iris")) %>%
     req_url_query(limit = 5)
 
   dir <- withr::local_tempdir()
@@ -32,8 +30,7 @@ test_that("can save results to disk", {
 })
 
 test_that("user temination still returns data", {
-  req <- request(example_url()) %>%
-    req_url_path("/iris") %>%
+  req <- request(example_url("/iris")) %>%
     req_url_query(limit = 5)
   next_req <- function(resp, req) interrupt()
 
@@ -45,8 +42,7 @@ test_that("user temination still returns data", {
 
 
 test_that("can retrieve all pages", {
-  req <- request(example_url()) %>%
-    req_url_path("/iris") %>%
+  req <- request(example_url("/iris")) %>%
     req_url_query(limit = 1)
 
   i <- 1
--- tests/testthat/test-resp-url.R
+++ tests/testthat/test-resp-url.R
@@ -1,7 +1,7 @@
 test_that("can extract url components from a response", {
   resp <- req_perform(request_test("/get?a=1&b=2"))
 
-  expect_equal(resp_url(resp), paste0(example_url(), "get?a=1&b=2"))
+  expect_equal(resp_url(resp), example_url("/get?a=1&b=2"))
   expect_equal(resp_url_path(resp), "/get")
   expect_equal(resp_url_queries(resp), list(a = "1", b = "2"))
 
