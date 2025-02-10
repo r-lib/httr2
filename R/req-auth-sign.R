4d30fcff39472dc8023f572bc665ce685856fb72
Hadley Wickham <h.wickham@gmail.com>
2025-02-10 13:55:46 -0600

    Explicitly expose cache object in `req_auth_sign()` (#676)

--- R/req-auth-sign.R
+++ R/req-auth-sign.R
@@ -1,20 +1,25 @@
 
-req_auth_sign <- function(req, fun, params) {
+req_auth_sign <- function(req, fun, params, cache) {
   req_policies(req,
     auth_sign = list(
       fun = fun,
-      params = params
+      params = params,
+      cache = cache
     )
   )
 }
-auth_sign <- function(req, reauth = FALSE) {
+auth_sign <- function(req) {
   if (!req_policy_exists(req, "auth_sign")) {
     return(req)
   }
 
   exec(req$policies$auth_sign$fun,
     req = req,
-    reauth = reauth,
+    cache = req$policies$auth_sign$cache,
     !!!req$policies$auth_sign$params
   )
+}
+
+req_auth_clear_cache <- function(req) {
+  req$policies$auth_sign$cache$clear()
 }
