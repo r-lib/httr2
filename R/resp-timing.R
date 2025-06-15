2de29c0c9f84e2dd41fcc9af716daffe085aad3f
Carl Suster <carl.suster@sydney.edu.au>
2025-06-16 09:51:33 +1000

    Improve resp_timing docs

--- R/resp-timing.R
+++ R/resp-timing.R
@@ -1,19 +1,17 @@
-#' Extract information about the timing of the response
+#' Extract timing data
 #'
 #' The underlying curl library measures how long different components of the
 #' request take to complete. This function retrieves that information.
 #'
-#' The names of the elements in this vector correspond to the names used
-#' in libcurl's `curl_easy_getinfo()` API. For example, `"namelookup"` is the
-#' time returned by requesting `CURLINFO_NAMELOOKUP_TIME`. Refer to [curl
-#' documentation] for explanations of what these measure. The most useful
-#' component is likely `"total"`, the overall time in seconds to complete the
-#' request including any redirects followed.
-#'
-#' [curl documentation]: https://curl.se/libcurl/c/curl_easy_getinfo.html
-#'
 #' @inheritParams resp_header
-#' @returns named numeric vector of timing information
+#' @returns Named numeric vector of timing information.
+#'  The names of the elements in this vector correspond to the names used
+#'  in [libcurl's `curl_easy_getinfo()` API][curl docs].
+#'  The most useful component is likely `"total"` (corresponding to
+#'  `CURLINFO_TOTAL_TIME`), the overall time in seconds to complete the
+#'  request including any redirects followed.
+#'
+#'  [curl docs]: https://curl.se/libcurl/c/curl_easy_getinfo.html
 #' @export
 #' @examples
 #' req <- request(example_url())
--- tests/testthat/test-resp-timing.R
+++ tests/testthat/test-resp-timing.R
@@ -1,4 +1,4 @@
-test_that("can extract request", {
+test_that("can extract request timing", {
   req <- request_test()
   resp <- req_perform(req)
   expect(is_bare_numeric(resp_timing(resp)), "timing vector is not numeric")
