aa6e81e8e3e88d4034ace26005dc6f6973788d5a
Constantinos Giachalis <cgiachalis@outlook.com>
2026-01-14 11:20:36 +0200

    'application/problem+json' works with `last_response_json()`
    closes #782

--- R/last.R
+++ R/last.R
@@ -61,7 +61,9 @@ last_response_json <- function(pretty = TRUE) {
   if (is.null(resp)) {
     cli::cli_abort("No request has been made successfully yet.")
   }
-  if (!identical(resp_content_type(resp), "application/json")) {
+  content_type <- resp_content_type(resp)
+  if (!(identical(content_type, "application/json") ||
+      identical(content_type, "application/problem+json"))) {
     cli::cli_abort("Last response doesn't have a JSON body.")
   }
   httr2_json(resp_body_string(resp), pretty = pretty)
